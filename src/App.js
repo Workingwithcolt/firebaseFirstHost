d949636cc5ba21f83403633bcd0b374fde865fc3
WorkingwithColt <mohitechetan2019@gmail.com>
2024-11-06 22:46:04 +0530

    c

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography'
 function App() {
   return (
     <Grid2 container>
-      <Typography variant="body1" className='text-center' color="initial">Todo App</Typography>
+      <Typography variant="body1" className='text-center' color="initial">Todo Appsdsdsd</Typography>
       <img src={'/neom-0SUho_B0nus-unsplash.jpg'} width={500} alt='' height={500}></img>
     </Grid2>
   );
