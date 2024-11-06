53756efd5c0fe9364c101f03cccbccf30e13f539
WorkingwithColt <mohitechetan2019@gmail.com>
2024-11-06 22:30:35 +0530

    c  updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import Typography from '@mui/material/Typography'
+
 
 function App() {
   return (
-    <Grid2>
-      Hellow Chetana
+    <Grid2 container>
+      <Typography variant="body1" className='text-center' color="initial">Todo App</Typography>
+      <img src={'/neom-0SUho_B0nus-unsplash.jpg'} width={500} height={500}></img>
     </Grid2>
   );
 }
